af784bd0dd86f7f76d8d1b8034aeef92a60134ee
酒MT <xyfqldy@163.com>
2022-10-05 00:17:16 +0800

    1

--- 2.10.0/crash_issue_survey/lib/main.dart
+++ 2.10.0/crash_issue_survey/lib/main.dart
@@ -2,3 +2,5 @@
 /// please run main entry point in :
 ///   1.  crash_issue_survey\lib\example_with_cachenetworkimage\main.dart
 ///   1.  crash_issue_survey\lib\example_without_cachenetworkimage\main.dart
+
+// if Image resources server  error , please replace Image sources
