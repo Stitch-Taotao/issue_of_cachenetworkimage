d70a1137aa73f502ebb8a92715e1773b4abb4a20
酒MT <xyfqldy@163.com>
2022-10-05 00:17:41 +0800

    1

--- 2.10.0/crash_issue_survey/lib/main.dart
+++ 2.10.0/crash_issue_survey/lib/main.dart
@@ -3,4 +3,4 @@
 ///   1.  crash_issue_survey\lib\example_with_cachenetworkimage\main.dart
 ///   1.  crash_issue_survey\lib\example_without_cachenetworkimage\main.dart
 
-// if Image resources server  error , please replace Image sources
+// if Image resources server  error , please replace Image sources with yours
